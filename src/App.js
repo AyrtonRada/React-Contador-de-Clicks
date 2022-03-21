69d1eb498b1c49ab25a553c7af34ee2dac0dc4a3
AyrtonRada <axel582011@hotmail.com>
2022-03-21 17:55:11 -0300

    botones y fondo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import logo from './imagenes/descargar.jpg'
+import Boton from './componentes/Boton'
+import Contador from './componentes/Contador'
+import { useState } from 'react'
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0)
+
+  const manejarClic = () =>{
+    setNumClics (numClics + 1) 
+  }
+
+  const reiniciarContador = () =>{
+    setNumClics(0)
+  }
+
   return (
     <div className="App">
-      
+      <div className='logo-contenedor'>
+        <img className='logo' src={logo} alt='este es un logo'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+      </div>
+      <div className='contenedor-boton'>
+        <Boton texto='Clic' esBotonDeClic={true} manejarClic={manejarClic}/>
+        <Boton texto='Reiniciar' esBotonDeClic={false} manejarClic={reiniciarContador}/>
+      </div>
     </div>
   );
 }
